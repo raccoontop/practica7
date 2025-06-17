4cdaab9f4463d770cc8d138920725ff0c7a51e1e
Max <evimovm9@gmail.com>
2025-06-18 01:37:58 +0300

    Initial commit - practica7 Spring Boot project

---
+++ src/main/java/com/example/practica7/Practica7Application.java
@@ -0,0 +1,13 @@
+package com.example.practica7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practica7/Practica7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
